fd508af1eecd1d2a6df87f613b2b0c4382e1d791
K Abhi Ram Reddy <abhiramreddy123.k@gmail.com>
2026-01-27 18:04:06 +0530

    Add level 2,3 and 4 task folders

---
+++ Cognifyz-Android-Internship/Level 2/Task3_ListDisplay/app/src/main/java/com/example/task3_listdisplay/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.task3_listdisplay;
+
+import android.os.Bundle;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView=findViewById(R.id.taskListView);
+
+        String[]tasks={
+                "Task 1:Hello World App",
+                "Task 2:Button interaction",
+                "Task 3:List Display",
+                "Task 4:Basic UI Layout",
+                "Task 5:Fetch & Display Data",
+                "Task 6:Simple Form",
+                "Task 7:Hello World App",
+                "Task 8:Hello World App"
+        };
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                tasks
+        );
+        listView.setAdapter(adapter);
+    }
+}
